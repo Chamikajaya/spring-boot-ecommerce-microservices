2c07e7955f8ae469bb2e044387b043fc9e6163b2
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2024-07-20 22:40:03 +0530

    added the yml configs for payment service

--- services/payment/src/main/resources/db/migration/V3__create_tables.sql
+++ services/payment/src/main/resources/db/migration/V3__create_tables.sql
@@ -1,14 +1,13 @@
-
 -- Create t_payment table
 CREATE TABLE IF NOT EXISTS t_payment
 (
-    id              INTEGER         NOT NULL DEFAULT nextval('payment_id_seq') PRIMARY KEY,
-    reference       VARCHAR(255)    NOT NULL UNIQUE,
-    payment_method  payment_method  NOT NULL,
-    amount    NUMERIC(10, 2)  NOT NULL CHECK (amount >= 0),
-   payment_status BOOLEAN         NOT NULL ,
-    order_id        INTEGER         NOT NULL,
-    created_at      TIMESTAMP       NOT NULL,
-    updated_at      TIMESTAMP       NOT NULL
+    id             INTEGER        NOT NULL DEFAULT nextval('payment_id_seq') PRIMARY KEY,
+    reference      VARCHAR(255)   NOT NULL UNIQUE,
+    payment_method payment_method NOT NULL,
+    amount         NUMERIC(10, 2) NOT NULL CHECK (amount >= 0),
+    payment_status BOOLEAN        NOT NULL,
+    order_id       INTEGER        NOT NULL,
+    created_at     TIMESTAMP      NOT NULL,
+    updated_at     TIMESTAMP      NOT NULL
 );
 
